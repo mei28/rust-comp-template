7f95dc1d394b97364b0ed7c30e35ce59426d6ccb
mei <mei28aquarius@gmail.com>
2023-05-14 11:29:06 +0900

    Add initial Rust template files for bins a, b, c, and d with basic IO setup

---
+++ src/bin/d.rs
@@ -0,0 +1,14 @@
+use cmp::{max, min, Reverse};
+use proconio::{fastout, input, marker::*};
+use std::collections::*;
+use std::*;
+
+const INF: usize = 1000000;
+
+fn main() {
+    input! {
+        n: usize,
+    }
+
+    println!("{}", n);
+}
